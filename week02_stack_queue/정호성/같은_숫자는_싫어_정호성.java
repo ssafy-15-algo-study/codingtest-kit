7703ed56b33767b948105fc06591796b6281edb7
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-08-23 21:44:31 +0900

    solve: 같은 숫자는 싫어 (정호성)

--- week02_stack_queue/정호성/같은_숫자는_싫어_정호성.java
+++ week02_stack_queue/정호성/같은_숫자는_싫어_정호성.java
@@ -2,11 +2,19 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        int[] answer = {};
+        List<Integer> answer = new ArrayList<>();
         
+        answer.add(arr[0]);
+        
+        for(int i = 1; i < arr.length; ++i) {
+            if(arr[i] == answer.get(answer.size() - 1)) {
+                continue;
+            }
+            
+            answer.add(arr[i]);
+        }
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
 
-        return answer;
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
